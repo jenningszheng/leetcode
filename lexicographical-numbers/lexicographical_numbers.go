50fed279badeacf98d081c464b5f370fbf1793a0
zhengjianning <zhengjianning@kuaishou.com>
2022-04-18 14:53:09 +0800

    update

---
+++ lexicographical-numbers/lexicographical_numbers.go
@@ -0,0 +1,22 @@
+package lexicographical_numbers
+
+func LexicalOrder(n int) []int {
+	ans := make([]int, n)
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num * 10 <= n {
+			num = num * 10
+		} else {
+			for {
+				if num%10 == 9 || num+1 > n {
+					num = num / 10
+					continue
+				}
+				break
+			}
+			num++
+		}
+	}
+	return ans
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"leetcode/lexicographical-numbers"
+)
+
+func main() {
+	ans := lexicographical_numbers.LexicalOrder(10)
+	fmt.Println(ans)
+}
